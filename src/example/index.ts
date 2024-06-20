feaec41edb762e23c74f216bfcba7229e1804800
cmidgley <chris@Koose.com>
2024-06-20 09:04:24 -0400

    Rename js files to ts

---
+++ src/example/index.ts
@@ -0,0 +1,3 @@
+// we use "../example" instead of just "." because moddable needs to see the parent folder name for
+// module resolution to work
+export { doSomething } from '../example/do-something.js';
