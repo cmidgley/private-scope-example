6b3fb8a5e828a2ba6116bca0139948ec9939586e
cmidgley <chris@Koose.com>
2024-06-20 11:22:10 -0400

    minor cleanup

--- src/example/index.js
+++ src/example/index.js
@@ -1,3 +1,3 @@
-// we use "../examples" instead of just "." beause moddable needs to see the parent folder name for
+// we use "../example" instead of just "." because moddable needs to see the parent folder name for
 // module resolution to work
 export { doSomething } from '../example/do-something.js';
